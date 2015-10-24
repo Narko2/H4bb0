91ee8382df4b05a6bab2c607c6c9a0a7461052a9
ThomKiesewetterPrive <thom@kiesewetter.nl>
2015-10-24 13:54:34 +0200

    Init version

---
+++ Habbo Hax Pack v02/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Habbo_Hax_Pack_v02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.ForeColor = SystemColors.GrayText;
+            textBox2.Text = "Here your password";
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Length == 0)
+            {
+                textBox2.Text = "Here your password";
+                textBox2.ForeColor = SystemColors.GrayText;
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Here your password")
+            {
+                textBox2.Text = "";
+                textBox2.ForeColor = SystemColors.WindowText;
+            }
+        }
+    }
+}
